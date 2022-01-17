29fede456fdf8a938fd5094c074541bce532bab0
Dipesh Joshi <joshidipesh12@gmail.com>
2022-01-17 09:44:45 +0530

    conerted me to webp

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,9 @@ export default function Home() {
       <Head>
         <title>Hola amigos ✨🎀</title>
         <meta name="description" content="Hi there, I'm Dipesh" />
+        <meta name="mobile-web-app-capable" content="yes"/>
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -42,7 +45,7 @@ export default function Home() {
                 priority={true}
                 layout="fill"
                 objectFit="contain"
-                src="/me.png"
+                src="/me.webp"
                 alt="das me!"
                 loading="eager"
                 onLoadingComplete={() => {
