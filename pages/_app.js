e89ef32810db5d6260045e63dcbd9bee88b3c0e4
Dipesh Joshi <joshidipesh12@gmail.com>
2022-06-10 19:05:58 +0530

    --vh updation updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,18 @@
 import '../styles/globals.css';
 import {useEffect} from 'react';
-import {motion, AnimatePresence, useAnimation} from 'framer-motion';
+import {motion, AnimatePresence} from 'framer-motion';
 
 function MyApp({Component, pageProps, router}) {
-  const controls = useAnimation();
-
   useEffect(() => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.documentElement.style.setProperty(
+      '--vh',
+      `${window.innerHeight * 0.01}px`,
+    );
     window.addEventListener('resize', () => {
-      let vh1 = window.innerHeight * 0.01;
-      controls.start({'--vh': vh1});
-      vh = vh1;
+      document.documentElement.style.setProperty(
+        '--vh',
+        `${window.innerHeight * 0.01}px`,
+      );
     });
   }, []);
 
@@ -20,7 +21,7 @@ function MyApp({Component, pageProps, router}) {
       <motion.div
         layout
         key={router.route}
-        transition={{when: 'afterChildren'}}
+        transition={{}}
         initial={{opacity: 0}}
         animate={{opacity: 1}}
         exit={{opacity: 0}}>
