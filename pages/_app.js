d4af5087869118662908e03d298dcf72a35727b3
Dipesh Joshi <joshidipesh12@gmail.com>
2022-06-17 11:40:30 +0530

    content heading: projects

--- pages/_app.js
+++ pages/_app.js
@@ -10,15 +10,8 @@ function MyApp({Component, pageProps, router}) {
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', function () {
         navigator.serviceWorker.register('/sw.js').then(
-          function (registration) {
-            console.log(
-              'Service Worker registration successful with scope: ',
-              registration.scope,
-            );
-          },
-          function (err) {
-            console.log('Service Worker registration failed: ', err);
-          },
+          res => {},
+          err => console.log('Service Worker registration failed: ', err),
         );
       });
     }
