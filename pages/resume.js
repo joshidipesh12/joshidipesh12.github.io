f523b4adfcf5d265b83fd6924436e241b41f4869
Dipesh Joshi <joshidipesh12@gmail.com>
2022-10-04 06:55:31 +0530

    updated resume link

--- pages/resume.js
+++ pages/resume.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {Head, Bottom, Loader, Navbar} from '../components';
+import React, { useState } from 'react';
+import { Head, Bottom, Loader, Navbar } from '../components';
 import styles from '../styles/Resume.module.css';
 
 function Resume() {
@@ -8,7 +8,7 @@ function Resume() {
   return (
     <div className={styles.main}>
       <Head title="Resume" />
-      <Navbar containerStyle={{position: 'sticky', top: 0}} />
+      <Navbar containerStyle={{ position: 'sticky', top: 0 }} />
       <Loader visible={loading} />
       <div className={styles.resume}>
         <iframe
@@ -16,7 +16,7 @@ function Resume() {
           onLoad={e => setLoading(false)}
           className={styles.pdf}
           // src="https://docs.google.com/document/d/e/2PACX-1vQs9FdG0TZFr8F3A06znseyRHyAKCnsvZ9NR553iTCKoOFLWhV_RZgLqOwnd8hYV_fXg-eYu5QJKLqe/pub?embedded=true"
-          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FQzs0kvGeQeRyf7BR1JgvCz%2FDIPESH-JOSHI---CV%3Fnode-id%3D399%253A2"
+          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FQzs0kvGeQeRyf7BR1JgvCz%2FDIPESH-JOSHI---CV%3Fnode-id%3D1119%253A3"
           allowFullScreen></iframe>
       </div>
       <Bottom />
